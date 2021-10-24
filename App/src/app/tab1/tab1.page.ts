3c206f25b2606b4ffdd18be0afbfa3690e0ad170
Caleb Hernández <tescorporations@gmail.com>
2021-10-23 22:51:37 -0500

    Se agrego biblioteca para graficas

--- App/src/app/tab1/tab1.page.ts
+++ App/src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,6 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
   constructor() {}
 
 }
